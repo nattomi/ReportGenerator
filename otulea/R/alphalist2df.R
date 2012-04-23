757d745bd97e63045338d651024727deca1d79d7
nattomi <nattomi@elnyuhetetlen.(none)>
2012-04-23 12:34:59 +0200

    alphalist2df modified in a way that now it accept a character string as an argument not just a parsed xml class

--- otulea/R/alphalist2df.R
+++ otulea/R/alphalist2df.R
@@ -3,13 +3,15 @@
 ## EXAMPLE
 if (FALSE) {
 require(XML)
-alphalistXML <- system.file("www/data/item/alphalist/alphalist.XML",package="otulea")
-docTree <- xmlInternalTreeParse(alphalistXML)
+alphalist <- system.file("www/data/item/alphalist/alphalist.XML",package="otulea")
+alphalist2df(alphalist)
+docTree <- xmlInternalTreeParse(alphalist)
 alphalist2df(docTree)
 }
 
-alphalist2df <- function(docTree) {
-  alphalist.list <- xmlToList(docTree)
+alphalist2df <- function(alphalist) {
+  if (is.character(alphalist)) alphalist <- xmlInternalTreeParse(alphalist)
+  alphalist.list <- xmlToList(alphalist)
   alphalist.list <- alphalist.list[names(alphalist.list)=="alphanode"]
   ans <- data.frame(t(as.data.frame(alphalist.list)),stringsAsFactors=F)
   rownames(ans) <- NULL
