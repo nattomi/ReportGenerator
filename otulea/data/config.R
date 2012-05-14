08c205577b11ada1c470e4556423aff3ad07111a
nattomi <nattomi@elnyuhetetlen.(none)>
2012-05-14 14:00:56 +0200

    package description corrected and config file added to data folder

---
+++ otulea/data/config.R
@@ -0,0 +1,7 @@
+## Configure directories
+packagePath <- find.package("otulea", lib.loc=NULL, quiet = TRUE) ## this line is not needed if file paths are hard-coded
+## edit the right sides before installing the package
+usersDir <- file.path(packagePath,"www/data/user") 
+alphalist <- file.path(packagePath,"www/data/item/alphalist/alphalist.XML")
+## cleaning up
+rm(packagePath) ## not needed if paths are hard-coded
