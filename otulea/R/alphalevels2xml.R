3798fd831532e814150d4c47bbdcd6e558ae9c4d
nattomi <nattomi@elnyuhetetlen.(none)>
2012-05-17 18:36:49 +0200

    variable 'dimension' replaced with 'subject' for consistency and some typos fixed

--- otulea/R/alphalevels2xml.R
+++ otulea/R/alphalevels2xml.R
@@ -8,9 +8,9 @@ threshold <- 50 ## percentage
 maxListings <- 3 ## maximal number of results listed
 alphalevels <- getAlphalevels(user,threshold,maxListings)
 file <- "20120504_14_13_X0AT2.pdf"
-dimension <- 2
-level <- 1
-result <- alphalevels2xml(alphalevels,file,dimension, level)
+subject <- attributes(alphalevels)$subject
+level <- attributes(alphalevels)$level
+result <- alphalevels2xml(alphalevels,file,subject, level)
 result
 ## saving it to a file
 write(saveXML(result),file=tempfile())
@@ -18,17 +18,17 @@ write(saveXML(result),file=tempfile())
 
 
 ## FUNCTION
-alphalevels2xml <- function(alphalevels,file,dimension,level) {
+alphalevels2xml <- function(alphalevels,file,subject,level) {
   node <- newXMLNode("results")
   ## adding 'print' node
   newXMLNode("print",parent = node, attrs=c("file" = file))
   ## adding 'timestamp' node
   ts <- format(Sys.time(),"%Y%m%d%H%M%S")
   newXMLNode("timestamp", parent = node, attrs=c("order" = "YMDhms", "value" = ts))
-  ## adding 'dimension' node
-  newXMLNode("dimension", parent = node, attrs=c("dimension" = dimension))
+  ## adding 'subject' node
+  newXMLNode("subject", parent = node, attrs=c("value" = subject))
   ## adding 'level' node
-  newXMLNode("level", parent = node, attrs=c("level" = level))
+  newXMLNode("level", parent = node, attrs=c("value" = level))
   ## adding 'eval' nodes
   modes <- names(alphalevels)
   ##i <- 1
