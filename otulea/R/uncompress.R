f6817905be694f9d0d399b73b034ce27cb54eeb7
nattomi <nattomi@elnyuhetetlen.(none)>
2012-05-31 22:42:56 +0200

    function corrected

--- otulea/R/uncompress.R
+++ otulea/R/uncompress.R
@@ -41,17 +41,21 @@ uncprsd2tex <- function(x) {
   ##cat("\\usepackage[german]{babel}","\n") ## it might be useful for displaying quotation marks correctly
   cat("\\usepackage[top=2.5cm, bottom=2.5cm, left=2.5cm, right=2.5cm]{geometry}","\n")
   cat("\\begin{document}","\n")
+  ## I want no page numbering
+  cat("\\pagestyle{empty}","\n")
   ## start of main content
-  lapply(x,function(x) {
-    cat("\\section*{What do I already know?}","\n")
-    if (dim(x)[1] > 0) {
-      tab <- xtable(x)
+  for (i in paste("A",1:3,sep="")) {
+    y <- x[[i]]
+    title <- as.character(titles$title[match(i,titles$mode)])
+    cat("\\section*{",title,"}","\n",sep="")
+    if (dim(y)[1] > 0) {
+      tab <- xtable(y)
       print(tab,include.rownames=FALSE)
      } else {
       cat("Nothing to display","\n")
     }
     cat("\\newpage","\n")
-  })
+  }
   ## end of main content
   cat("\\end{document}","\n")
 }
