f748a3bec73deae82ca3195d1f5d01a45f8e9cd3
nattomi <nattomi@elnyuhetetlen.(none)>
2012-07-16 17:26:50 +0200

    typo corrected

--- otulea/R/feedback2tex.R
+++ otulea/R/feedback2tex.R
@@ -6,14 +6,20 @@
 if (FALSE) {
 library(otulea)
 ##library(xtable)
-##library(tools)
+library(tools)
 user <- "6CKBT" ## user id as character string
 threshold <- 50 ## percentage
 maxListings <- 3 ## maximal number of results listed
 alphalist.df <- alphalist2df(alphalist)
 y <- getAlphalevels(user,threshold,maxListings,alphalist.df)
 x <- uncompress(y)$A1
+## this one just prints it to screen
 feedback2tex(x)
+## this one overwrites template.tex
+sink("../inst/feedback/tabular.tex")
+feedback2tex(x)
+sink()
+texi2dvi("../inst/feedback/userfeedback.tex",pdf=TRUE)
 }
 
 ## FUNCTION DEFINITION
@@ -29,7 +35,7 @@ feedback2tex <- function(x) {
   tbl <- xtable(x.sub)
   align(tbl) <- "cm{30pt}|m{310pt}|m{310pt}"
   ## commands to add and their position
-  command <- rep(c("\\rowcolor{green!10}","rowcolor{bule!10}"),length.out=rownum+1)
+  command <- rep(c("\\rowcolor{green!10}","\\rowcolor{blue!10}"),length.out=rownum+1)
   pos <- as.list(seq(-1,rownum-1))
   print(tbl,floating=FALSE,sanitize.text.function = function(x){x},
         include.rownames=FALSE,hline.after=0,
