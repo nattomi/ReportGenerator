584f2cfdbfd0be9519633530732028ef8ac0c35e
nattomi <nattomi@elnyuhetetlen.(none)>
2012-07-16 14:02:04 +0200

    function for creating the included table

--- otulea/R/feedback2tex.R
+++ otulea/R/feedback2tex.R
@@ -18,8 +18,22 @@ feedback2tex(x)
 
 ## FUNCTION DEFINITION
 feedback2tex <- function(x) {
-  x.sub <- x[,c("userdescription","example")]
-  xtable(x.sub)
+  ## number of rows
+  rownum <- dim(x)[1]
+  ## subsetting just to make sure that the input is correct
+  ## plus adding 'speaker' column
+  x.sub <- cbind("\\speaker",x[,c("userdescription","example")])
+  colnames(x.sub) <- c("","\\scalebox{0.2}{\notepad} Schreiben","Beispiel")
+  ## xtable commands
+  tbl <- xtable(x.sub)
+  align(tbl) <- "cm{30pt}|m{310pt}|m{310pt}"
+  ## commands to add and their position
+  command <- rep(c("\\rowcolor{green!10}","rowcolor{bule!10}"),length.out=rownum+1)
+  pos <- as.list(seq(-1,rownum-1))
+  print(tbl,floating=FALSE,sanitize.text.function = function(x){x},
+        include.rownames=FALSE,hline.after=0,
+        add.to.row=list(pos=pos,command=command))
 }
 
 
+attributes(tbl)
