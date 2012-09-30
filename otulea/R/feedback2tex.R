2c11127978fd1431f4b99c0fc1805c71b09b072b
nattomi <nattomi@elnyuhetetlen.(none)>
2012-09-30 12:30:37 +0200

    further improvement of feedback layout

--- otulea/R/feedback2tex.R
+++ otulea/R/feedback2tex.R
@@ -40,6 +40,8 @@ setwd("../inst/feedback/")
 texi2pdf("userfeedback.tex")
 
 lapply(y, feedback2tex)
+bgcols=c("d8f2f1","ddf4dd")
+x <- x$A1
 }
 
 ## FUNCTION DEFINITION
@@ -58,19 +60,25 @@ feedback2tex <- function(x,bgcols=c("d8f2f1","ddf4dd")) {
       ## subsetting just to make sure that the input is correct
       ## plus adding 'speaker' column
       x.sub <- cbind("\\speaker",x[,c("userdescription","example")])
-      colnames(x.sub) <- c("","\\scalebox{0.2}{\notepad} Schreiben","Beispiel")
+      colnames(x.sub) <- c("",sj,"Beispiel")
       ## xtable commands
       tbl <- xtable(x.sub)
-      align(tbl) <- "cm{30pt}|m{310pt}|m{310pt}"
+      align(tbl) <- "c|m{90pt}|m{290pt}|m{290pt}|"
       ## commands to add and their position
       command1 <- rep(c("\\rowcolor{bgcol1}","\\rowcolor{bgcol2}"),length.out=rownum+1)
-      command2 <- rep("",length.out=rownum+1)
-      command2[2] <- "\\hline"
-      command <- paste(command2,command1)
+      command2 <- rep("\\hline",length.out=rownum+1)
+      #command2[2] <- "\\hline"
+      command2[1] <- ""
+      command1
+      command2
+      command <- paste(command1,command2)
+      command
+      
       pos <- as.list(seq(-1,rownum-1))
+      rownum
       print(tbl,floating=FALSE,sanitize.text.function = function(x){x},
             include.rownames=FALSE,
-            hline.after=NULL,
+            hline.after=c(unlist(pos),rownum),
             add.to.row=list(pos=pos,command=command))
       options(op)
     } else {
