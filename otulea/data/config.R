8e55566bb674f2fe7e7ae931a62311f25fced1c3
nattomi <nattomi@apad.(none)>
2013-04-21 21:03:18 +0200

    config file modified

--- otulea/data/config.R
+++ otulea/data/config.R
@@ -3,6 +3,10 @@ packagePath <- find.package("otulea", lib.loc=NULL, quiet = TRUE) ## this line i
 ## edit the right sides before installing the package
 usersDir <- file.path(packagePath,"www/data/user") 
 alphalist <- file.path(packagePath,"www/data/item/alphalist/alphalist.XML")
+templateFiles <- c(haken="www/feedback/template/haken.pdf",
+                                         leiter="www/feedback/template/leiter.pdf",
+                                         userfeedback="www/feedback/template/userfeedback.tex")
+templateFiles <- sapply(templateFiles,function(x) file.path(packagePath,x))
 ## cleaning up
 rm(packagePath) ## not needed if paths are hard-coded
 ## On my server, I need the following settings
