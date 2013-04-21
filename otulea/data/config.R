d1f94bb30f6c409dfc9bf40dc2539afb13b721e1
nattomi <nattomi@apad.(none)>
2013-04-21 21:31:40 +0200

    config file updated

--- otulea/data/config.R
+++ otulea/data/config.R
@@ -1,11 +1,11 @@
 ## Configure directories
 packagePath <- find.package("otulea", lib.loc=NULL, quiet = TRUE) ## this line is not needed if file paths are hard-coded
 ## edit the right sides before installing the package
-usersDir <- file.path(packagePath,"www/data/user") 
-alphalist <- file.path(packagePath,"www/data/item/alphalist/alphalist.XML")
-templateFiles <- c(haken="www/feedback/template/haken.pdf",
-                                         leiter="www/feedback/template/leiter.pdf",
-                                         userfeedback="www/feedback/template/userfeedback.tex")
+usersDir <- file.path(packagePath,"data/user") 
+alphalist <- file.path(packagePath,"data/item/alphalist/alphalist.XML")
+templateFiles <- c(haken="template/haken.pdf",
+                   leiter="template/leiter.pdf",
+                   userfeedback="template/userfeedback.tex")
 templateFiles <- sapply(templateFiles,function(x) file.path(packagePath,x))
 ## cleaning up
 rm(packagePath) ## not needed if paths are hard-coded
