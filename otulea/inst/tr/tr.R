a69d48905af8e0d143aea151ba773639fbad2275
Nagy Tamás <nattomi@ggki.hu>
2014-02-02 10:41:52 +0100

    working version for table layout generation, no cell coloring

--- otulea/inst/tr/tr.R
+++ otulea/inst/tr/tr.R
@@ -1,59 +1,92 @@
+#!/usr/bin/Rscript
 ## SETTINGS
-##green <- rgb(193,255,193)
-##yellow <- rgb(255,246,143)
-##blue <- rgb(135,216,255)
-x <- tr$Schreiben
-
-table.print <- function(x) {
-  l <- length(x$aufgabe) 
-  cat("\\begin{tabularx}{\\textwidth}{@{}X@{}}\n")
+X <- c("Lesen","Schreiben","Sprache","Mathe")
+Y <- c("0")
+## ROUTINES
+multirow <- function(x) {
+  l <- length(x)
+  ans <- rep(0,l)
+  y <- c(".",x)
+  j <- 1
   for (i in 1:l) {
-    cat("  \\hline\n") # start of outer cycle
-    symb <- switch(x$tendency[i],down="$\\Downarrow$",constant="$\\Rightarrow$",up="$\\Uparrow$","")
-    cat("  \\rowcolor{",x$color[i],"}[0pt][0pt]",x$aufgabe[i]," ",symb,"\\\\\n",sep="")
-    cat("  \\hline\n")
-    ll <- length(x$description[[i]])
-    for (j in 1:ll) {
-      cat("  \\rowcolor{",x$color[i],"}[0pt][0pt]$\\bullet$ ",x$description[[i]][j],"\\\\\n",sep="")
-    }
-    cat("  \\hline\n")
+    if (y[i+1]!=y[i]) j <- i
+    ans[j] <- ans[j]+1
   }
-  cat("\\end{tabularx}\n")
+  ans
 }
 
-Lesen <- c("1.2.1","1.2.2","1.3.2","1.3.3","1.3.4")
-Lesen.description <- list(c("Kann Zeitpläne sinnentnehmend lesen (Dokument literacy).","Komplexität (Konsonantenhäufungen) recodieren und dekodieren."),"Kann Wörter mit ansteigender Komplexität (Konsonantenhäufung) recodieren und decodieren.", "Kann Wörter mit ansteigender Komplexität (Konsonantenhäufung) recodieren und decodieren.",c("Kann Sätze mit ansteigender Länge sinnerfassend lesen.", "Kann SPO-Sätze und SPO-Sätze mit Einfügungen sinnerfassend lesen."),c("Kann einzelne Wörter im Satzkontext erlesen. Kann orthografisch komplexere Wörter erlesen.","Kann Sätze mit ansteigender Länge sinnerfassend lesen.","Kann SPO-Sätze und SPO-Sätze mit Einfügungen sinnerfassend lesen."))
-Lesen.color <- c("A",rep("B",4))
-Lesen.tendency <- c(rep(NA,5))
-Schreiben <- c("2.1.1","2.1.2","2.1.3","2.1.4","2.1.5","2.1.6")
-Schreiben.description <- list(c("Kann Wörter mit Silben, die aus einem Vokal oder einem Diphtong bestehen, schreiben.", "Kann Zahlen als Ziffern schreiben.", "Kann Wörter mit offenen Silben schreiben."),"Kann lautierte einzelne Laute verschriftlichen.","Kann Groß- und Kleinbuchstaben in Druckschrift unterscheiden.", "Kann Wörter mit Silben, die aus einem Vokal oder Diphtong bestehen schreiben (O-ma, Au-to)", "Kann Zahlen bis 20 als Zahl schrieben", "Kann in einem logographischen Zugriff Standardanreden wie „Liebe“ oder „Hallo“ groß schreiben.")
-Schreiben.color <- c(rep("A",4),rep("C",2))
-Schreiben.tendency <- c("up","up","constant","up","constant","constant")
-Sprache <- c("3.1.1","3.1.2","3.1.3","3.1.4","3.2.1","3.2.2","3.2.3")
-Sprache.description <- list("Kann prädikative Kongruenz im Präsens in einem kurzen Satz erkennen.","Kann verbale Kongruenz im Präsenz in einem kurzen Satz erkennen.", "Kann nominale Kongruenz in einem kurzen Satz erkennen.", "Kann den passenden Genetiv in einem kurzen Satz erkennen.", "Kann prädikative Kongruenz im Perfekt in einem mäßig kurzen Satz erkennen.", "Kann verbale Kongruenz im Perfekt in einem mäßig kurzen Satz erkennen.", "Kann nominale Kongruenz im Akkusativ in einem mäßig kurzen Satz erkennen")
-Sprache.color <- c(rep("A",3),"B",rep("C",3))
-Sprache.tendency <- c("up","constant","up","up","constant","constant","constant")
-Mathe <- c("4.1.1","4.1.2","4.1.3","4.1.4","4.2.1","4.2.3")
-Mathe.description <- list("Kann Mengen im Zehner-, Hunderter- und höheren Bereichen erfassen.", "Kann Ziffern und Zahlen 1-1000 erkennen.", "Kann Zahlen von 10 bis 100 erkennen.", "Kann Ziffern und Zahlen 100 bis 1000 erkennen.", "Kann Einer-Ziffern im Kopf addieren", c("Kann Einer- und Zehner-Zahlen im Kopf zusammen zählen", "Kann Zehner- und Einer-Zahlen im Kopf zusammen zählen."))
-Mathe.color <- c(rep("A",3),rep("C",3))
-Mathe.tendency <- c("constant","constant","up","down","constant","constant")
+multirow.rev <- function(x) {
+  ans <- rev(multirow(rev(x)))
+  for (i in seq_along(ans)) {
+    if (ans[i] > 1) ans[i] <- -ans[i]
+  }
+  ans
+}
 
-tr <- list(Lesen=list(aufgabe=Lesen,description=Lesen.description,color=Lesen.color,tendency=Lesen.tendency),
-           Schreiben=list(aufgabe=Schreiben,description=Schreiben.description,color=Schreiben.color,tendency=Schreiben.tendency),
-           Sprache=list(aufgabe=Sprache,description=Sprache.description,color=Sprache.color,tendency=Sprache.tendency),
-           Mathe=list(aufgabe=Mathe,description=Mathe.description,color=Mathe.color,tendency=Mathe.tendency))
-attributes(tr) <- c(attributes(tr),list(participant="XQAT2", date=as.Date("2012-2-14")))
+cl <- function(x) {
+  num <- seq_along(x)[x]
+  paste("\\cline{",num,"-",num,"}",sep="",collapse="")
+}
+##CLINE
+##cline(CLINE[4,])
+## MAIN
+dat0 <- read.csv("tr.csv")
+dat <- transform(dat0,ast=paste(aufgabe,score,sep="_"),nch=nchar(as.character(description)))
+##head(dat)
 
-## printing stuff
-sink("Lesen.tex")
-table.print(tr$Lesen)
-sink()
-sink("Schreiben.tex")
-table.print(tr$Schreiben)
-sink()
-sink("Sprache.tex")
-table.print(tr$Sprache)
-sink()
-sink("Mathe.tex")
-table.print(tr$Mathe)
-sink()
+state.list <- by(dat,dat$state,function(x) x)
+## printing table
+cat("\\begin{tabular}{|p{.02\\textwidth}|p{.2\\textwidth}|p{.02\\textwidth}|p{.2\\textwidth}|p{.02\\textwidth}|p{.2\\textwidth}|p{.02\\textwidth}|p{.2\\textwidth}|}\n")
+##cat("\\hline\n")
+##state <- state.list[[1]]
+##y <- Y[1]
+for (y in Y) {
+  state <- state.list[[y]]
+  cat("\\hline\n")
+  dimension.list <- by(state,state$type,function(x) {
+    y <- x[,-match(c("type","aufgabe","score","state"),names(x))]
+    y
+  })
+  M <- sapply(dimension.list,function(x) dim(x)[1])
+  K <- max(M)-M
+  TABL.list <- lapply(X,function(y) {
+    tabl <- dimension.list[[y]]
+    repl <- K[[y]]
+    if (repl > 0) {
+      i <- which.max(tabl$nch)
+      tabl.i <- tabl[i,]
+      for (j in 1:repl) {
+        tabl <- rbind(tabl,tabl.i)
+      }
+    }
+    tabl <- tabl[order(tabl$ast,tabl$description),]
+    tabl[,c("ast","description")]
+  })
+  TABL <- do.call("cbind",TABL.list)
+  TABL.multirow <- as.data.frame(lapply(TABL,multirow))
+  d <- dim(TABL)
+  CLINE <- matrix(FALSE,nrow=d[1],ncol=d[2])
+  for (i in 1:d[1]) {
+    cline <- "\\..."
+    for (j in 1:d[2]) {
+      cellcontent <- " "
+      nrows <- TABL.multirow[i,j]
+      if (nrows>0) CLINE[nrows+i-1,j] <- TRUE
+      cellcontent.ij <- as.character(TABL[i,j])
+      cellcontent.ij <- strsplit(cellcontent.ij,"_")[[1]][1]
+      if (nrows > 0) {
+        pwidth <- ifelse(j %% 2==0,0.2,0.02)
+        cellcontent <- paste("\\parbox{",pwidth,"\\textwidth}{",cellcontent.ij,"}",sep="")
+        if (nrows > 1) {
+          cellcontent <- paste("\\multirow{",nrows,"}{*}{",cellcontent,"}",sep="")
+        }
+      }
+      cat(cellcontent)
+      separator <- ifelse(j < d[2]," & "," \\\\\n")
+      cat(separator)
+    }
+    cat(cl(CLINE[i,]),"\n")
+  }
+  cat("\\hline\n")
+}
+cat("\\end{tabular}\n")
