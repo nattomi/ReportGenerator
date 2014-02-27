d7356e259d6a5b41006e18c06dad5602844098e8
Nagy Tamás <nattomi@ggki.hu>
2014-02-27 08:44:32 +0100

    new format of result xml file

--- otulea/R/alphalevels2xml.R
+++ otulea/R/alphalevels2xml.R
@@ -7,12 +7,14 @@ user <- "6CKBT" ## user id as character string
 threshold <- 50 ## percentage
 maxListings <- 3 ## maximal number of results listed
 alphalist.df <- alphalist2df(alphalist)
-x <- getAlphalevels(user,threshold,maxListings,alphalist.df)
+x <- getAlphalevelsA(user,threshold,maxListings,alphalist.df)
+x
 uncprsd <- uncompress(x,alphalist.df,c("userdescription","example","sound","alphaID"))
 
 file <- "20120504_14_13_X0AT2.pdf"
 subject <- attr(x,"subject")
 level <- attr(x,"level")
+uncprsd
 result <- alphalevels2xml(uncprsd,file,subject, level)
 result
 ## saving it to a file
@@ -38,15 +40,8 @@ alphalevels2xml <- function(uncprsd,file,subject,level) {
   for (mode in modes) {
     nodeName <- paste("mode",mode,sep=".")
     assign(nodeName, newXMLNode("eval", parent = node, attrs=c("mode" = mode)))
-    ## innen kell módosítani
-    ##uncprsd[[mode.i]]
-    ##trans <- function(x,y) pastenewXMLNode("alphannode", parent=',nodeName,', attrs=c("alphaID" = y)))',sep="")
-
-    ##(uncprsd[[mode.i]],function(x) paste())
     dat <- uncprsd[[mode]]
-    ##length(t(dat))
-    ##as.data.frame(t(dat))
-    text <- paste('lapply(as.data.frame(t(dat)),function(x) newXMLNode("alphanode", parent=',nodeName,', attrs=c("alphaID" = as.character(x[["alphaID"]]),"sound" = as.character(x[["sound"]]))))',sep="")
+    text <- paste('lapply(as.data.frame(t(dat)),function(x) newXMLNode("alphanode", parent=',nodeName,', attrs=c("alphaID" = as.character(x[["alphaID"]]),"userdescription" = as.character(x[["userdescription"]]),"example" = as.character(x[["example"]]))))',sep="")
     eval(parse(text=text))
   }
   node
