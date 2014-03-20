51a421c99f93b4ddc5ceafaafe72f0a38cdc7e2a
Nagy Tamás <nattomi@ggki.hu>
2014-03-20 11:53:13 +0100

    replacing items with tasks

--- otulea/inst/teacherReport.R
+++ otulea/inst/teacherReport.R
@@ -6,7 +6,7 @@ Y <- c(solved="Kannbeschreibungen erfüllt",
        notsolved="Kannbeschreibungen nicht erfüllt")
 
 args <- commandArgs(TRUE)
-args <- "KFCG1" # this my test user
+##args <- "KFCG1" # this my test user
 if (length(args)==0) {
   cat("Usage: teacherReport.R user\n")
   cat("where\n")
@@ -35,10 +35,7 @@ if (length(args)==0) {
     tests
   }
   ## getting testresults
-  testResults <- function(user,last=FALSE) {
-      ## location of global user file
-    userDir <- file.path(usersDir,user)
-    guf <- file.path(userDir,paste(user,"xml",sep=".")) ## later on we rewrite this with system.file
+  testResults <- function(userDir,guf,last=FALSE) {
     ## tests taken by the user (returned as an XMLNodeSet)
     tests <- list.tests(guf)
     ## apply 'last' filter if requested
@@ -47,27 +44,31 @@ if (length(args)==0) {
     tests.df <- lapply(tests,test2df)
     testresults <- lapply(tests.df, function(x) {
       ans <- file.path(userDir,x$data)
-      attributes(ans) <- list(attrs=attributes(x)$attrs)
+      names(ans) <- x$iname
+      attributes(ans) <- c(attributes(ans),list(attrs=attributes(x)$attrs))
       ans})
     testresults
   }
   ## getting the marking sections of various testresult files
   ## and merge them into a data frame containing character strings
-  getMarkings <- function(testresults) {
-    markings <- lapply(testresults, function(x) {
+  getMarkings <- function(testresult) {
+    markings <- lapply(testresult, function(x) {
       x.parse <- xmlParse(x)
       x.marking <- getNodeSet(x.parse, "//marking")
       getNodeSet(x.marking[[1]],"//mark")
     })
-    markings.all <- do.call("c",markings)
-    markings.all2 <- lapply(markings.all,function(x) c(xmlAttrs(x)[c("itemnumber","alphalevel")],mark=xmlValue(x)))
-    markings.all3 <- lapply(markings.all2,function(x) {
-      m <- x[["mark"]]
-      if (m=="" | m=="failed") x[["mark"]] <- 0
-      x
-    })
-    markings.df <- as.data.frame(do.call("rbind",markings.all3))
-    attributes(markings.df)<- c(attributes(markings.df),attributes(testresults))
+    markings.all <- c()
+    for (n in names(markings)) {
+      markings.n <- markings[[n]]
+      ans.n <- lapply(markings.n,function(x) c(task=n,xmlAttrs(x)[c("itemnumber","alphalevel")],mark=xmlValue(x)))
+      ans.n <- do.call("rbind",ans.n)
+      markings.all <- rbind(markings.all,ans.n)
+    }
+    ## this is for sanitizing erronous data
+    marks <- markings.all[,"mark"]
+    markings.all[marks=="" | marks=="failed","mark"] <- 0
+    markings.df <- as.data.frame(markings.all)
+    attributes(markings.df)<- c(attributes(markings.df),attributes(testresult)$attrs)
     markings.df
   }
   ## converts alphalist.XML to a data.frame  
@@ -90,63 +91,71 @@ if (length(args)==0) {
   ## MAIN
   ## Layer 1: xml to tabular form
   user <- as.character(args[1])
-  testresults <- testResults(user)
+  userDir <- file.path(usersDir,user) # the user's folder
+  guf <- file.path(userDir,paste(user,"xml",sep=".")) # global user file
+  testresults <- testResults(userDir,guf)
   markings <- lapply(testresults,getMarkings)
   layer0 <- lapply(markings,function(x) {
-    rown=dim(x)[1]
-    a=attributes(x)$attrs
-    b=matrix(rep(a,rown),nrow=rown,byrow=TRUE)
+    rown <- nrow(x)
+    a <- attributes(x)[c("timestamp.string","subject.string","level.string")]
+    b <- matrix(rep(a,rown),nrow=rown,byrow=TRUE)
     colnames(b) <- names(a)
     cbind(b,x)
   })
-  ##layer0
-  layer1 <- do.call("rbind",layer0)[,c(c("timestamp.string","subject.string","level.string","itemnumber","alphalevel","mark"))]
-  ##layer1$mark[layer1$mark==""] <- 0 # we don't want any empty mark value
-  ##layer1$mark[layer1$mark=="failed"] <- 0 # we don't want "failed" as a mark value
-  layer1
+  layer0
+  layer1 <- do.call("rbind",layer0)[,c(c("timestamp.string","subject.string","level.string","itemnumber","alphalevel","mark","task"))]
   ## Layer 2: sort by subject,alphalevel,itemnumber, then report last 2 marks for each itemnumber
-  bysubject <- by(layer1[,c(1,4,5,6)],as.character(layer1$subject.string),function(x) x)
+  bysubject <- by(layer1[,c(1,7,4,5,6)],as.character(layer1$subject.string),function(x) x)
   ##head(bysubject$Lesen)
   bysubject
   layer2.list <- lapply(bysubject,function(x) { # sorts elements by subject
-    y <- by(x[,c(1,2,4)],as.character(x$alphalevel),function(x) x) # sorts elements belonging to a subject by ability description (alphalevel)
+    y <- by(x[,c(1,2,3,5)],as.character(x$alphalevel),function(x) x) # sorts elements belonging to a subject by ability description (alphalevel)
     lapply(y,function(x) { # calculating last two marks 
-      byitem <- by(x[,c(1,3)],as.character(x$itemnumber),function(x) {
+      byitem <- by(x[,c(1,2,4)],as.character(x$itemnumber),function(x) {
         x.time <- strptime(as.character(x$timestamp.string),format="%Y_%m_%d_%H_%M_%S",tz="GMT")
         x.time.bylatest <- order(x.time,decreasing=TRUE)
-        as.integer(as.character(x$mark[x.time.bylatest]))
+        x[x.time.bylatest,c("task","mark")]
       })
-      sapply(byitem,function(x) c(before.latest=x[2],latest=x[1]))
-    })
-  })
-  layer2.list$Lesen
-  layer2 <- lapply(layer2.list,function(z) { # calculating overall tendency and category for each alphalevel
-    lapply(z,function(y) {
-      tendencyvector <- apply(y,2,function(x) x[2]-x[1])
-      ans <- rbind(y,tendency=tendencyvector)
-      ## calculating the overall tendency
-      tv0 <- tendencyvector[!is.na(tendencyvector)]
-      tv <- ifelse(length(tv0) > 0,sum(tv0),NA)
-      ## add category as attribute
-      latest <- y["latest",]
-      ctg <- 10 
-      if (any(latest==0)) { 
-        ctg <- ifelse(all(latest==0),0,5)
-      }
-      attributes(ans) <- c(attributes(ans),list(ctg=ctg,tendency=sign(tv)))
+      ans <- sapply(byitem,function(x) {
+        c(latest=as.character(x[1,"mark"]),
+          before.latest=as.character(x[2,"mark"]),
+          task=as.character(x[1,"task"]))
+      })
       ans
     })
   })
+  layer2.list$Sprache
+  layer2 <- lapply(layer2.list,function(z) { # calculating overall tendency, category and checkmark for each alphalevel
+    lapply(z,function(y) {
+      ## calculating associated tasks and checkmarks
+      marks.latest <- as.integer(as.character(y["latest",]))
+      marks.before.latest <- as.integer(as.character(y["before.latest",]))
+      tasks <- tapply(marks.latest,y["task",],all)
+      ## adding tendency and category as attribute
+      tendencyvector <- marks.latest-marks.before.latest
+      ## calculating the overall tendency
+      tv0 <- tendencyvector[!is.na(tendencyvector)]
+      tv <- ifelse(length(tv0) > 0,sum(tv0),NA)
+      ## calculating the category attribute
+      ctg <- 10 
+      if (any(marks.latest==0)) { 
+        ctg <- ifelse(all(marks.latest==0),0,5)
+      }
+      ## return
+      attributes(tasks) <- c(attributes(tasks),list(ctg=ctg,tendency=sign(tv)))
+      tendencyvector
+      tasks
+    })
+  })
+  layer2$Schreiben
   ## group alphalevels by category
   layer3 <- lapply(layer2,function(x) lapply(c(solved=10,partly=5,notsolved=0),function(i) x[sapply(x,function(x) attributes(x)$ctg==i)]))
-  layer3
   systime <- Sys.time()
   baseName <- paste(user,format(systime,format="%Y%m%d_%H_%M_%S"),sep="_")
   baseName <- "systime" # for testing only
   baseName <- paste(baseName,"teacher",sep="_")
   pdfName <- paste(baseName,"pdf",sep=".")
   texName <- paste(baseName,"tex",sep=".")
-  userDir <- file.path(usersDir, user) # this one is quite redundant here, but quicker than rewriting a bunch of functions
   userDir <- "/tmp" # for testing
   tmpdir <- file.path(userDir,baseName)
   if (!file.exists(tmpdir)) dir.create(tmpdir) # just to make sure
@@ -159,20 +168,20 @@ if (length(args)==0) {
     cat("{\\scriptsize\\noindent")
     for (x in X) {
       cat("\\colorbox{",x,"-",catname,"}{\\begin{minipage}{.25\\textwidth}\n",sep="")
-      cat("\\hasab{\\begin{tabular}{@{\\hspace{.2em}}p{1em}@{\\hspace{.1em}}|@{\\hspace{.4em}}p{9.2em}@{\\hspace{.4em}}|@{\\hspace{.4em}}p{14em}@{\\hspace{.4em}}}\n")
+      cat("\\hasab{\\begin{tabular}{@{\\hspace{.2em}}p{1em}@{\\hspace{.1em}}|@{\\hspace{.4em}}p{14em}@{\\hspace{.4em}}|@{\\hspace{.4em}}p{9.2em}@{\\hspace{.4em}}}\n")
       cat("\\hline\n")
       cat("\\multicolumn{3}{c}{\\textbf{",x,"}}\\\\\n",sep="")
       cat("\\hline\n")
       cat("\\multicolumn{2}{@{\\hspace{.4em}}l|@{\\hspace{.4em}}}{\\textbf{Kannbeschreibung}} & \\textbf{Aufgabe} \\\\\n")
       cat("\\hline\n")
       xy <- layer3[[x]][[catname]]
-      item <- names(xy)[1]
+      ##item <- names(xy)[1] # for testing
       for (item in names(xy)) {
         i.alphaID <- match(item,alphalist.df$alphaID)
         tab <- xy[[item]]
         ctg <- attributes(tab)$ctg
-        CN <- colnames(tab)
-        checkmarks <- sapply(tab["latest",CN],function(x) ifelse(x,"\\checkmark",""))
+        CN <- names(tab)
+        checkmarks <- sapply(tab,function(x) ifelse(x,"\\checkmark",""))
         cell1 <- alphalist.df[i.alphaID,"description"]
         cell2 <- paste(paste(gsub("_","\\\\textunderscore ",CN),checkmarks,sep=""),collapse=", ")
         tend.int <- attributes(tab)$tendency
@@ -203,7 +212,7 @@ if (length(args)==0) {
   ## moving resulting pdf to user's dir and
   ## deleting temporary directory
   file.copy(file.path(tmpdir,pdfName),userDir,overwrite=TRUE)                            
-  unlink(tmpdir,recursive=TRUE)
+  ##unlink(tmpdir,recursive=TRUE)
   ## printing name of xml file to screen
   cat(pdfName)
 } # else
