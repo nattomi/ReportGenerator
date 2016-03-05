a516fc782f223421ed14337812b8a71af5e4e045
Tamàs Nagy <tamas@workforce.fb12.uni-bremen.de>
2016-03-05 19:09:42 +0100

    changes to catch up with the new api

--- tests/otulea__merge_items.php
+++ tests/otulea__merge_items.php
@@ -11,13 +11,13 @@ $ot->set_user("SD5AM");
 $timestamp = "2014_10_7_13_56_23";
 $tests = $ot->get_tests($timestamp);
 $test = $tests[$timestamp];
-$test_prev = $tests[$test->prev()];
+$test_prev = $tests[$test->get_prev()];
 
 // TEST 1
 echo "Test 1: 'data' is empty for both items: ";
 $item = "2.3.03_II";
-$item1 = $test->items()[$item];
-$item2 = $test_prev->items()[$item];
+$item1 = $test->get_items()[$item];
+$item2 = $test_prev->get_items()[$item];
 $merged = otulea::merge_items($item1, $item2);
 $cond1 = strcmp($merged['timestamp'], $timestamp) === 0;
 $cond2 = strlen($merged['data']) === 0;
@@ -30,10 +30,10 @@ echo PHP_EOL;
 // TEST 2
 echo "Test 2: 'data' is empty only for the first item: ";
 $item = '2.3.01_I';
-$item1 = $test->items()[$item];
-$item2 = $test_prev->items()[$item];
+$item1 = $test->get_items()[$item];
+$item2 = $test_prev->get_items()[$item];
 $merged = otulea::merge_items($item1, $item2);
-$cond1 = strcmp($merged['timestamp'], $test->prev()) === 0;
+$cond1 = strcmp($merged['timestamp'], $test->get_prev()) === 0;
 $cond2 = strcmp($merged['data'], $item2['data']) === 0;
 if($cond1 && $cond2)
   echo "OK";
@@ -44,8 +44,8 @@ echo PHP_EOL;
 // TEST 3
 echo "Test 3: 'data' is empty only for the second item: ";
 $item = '2.3.03_I';
-$item1 = $test->items()[$item];
-$item2 = $test_prev->items()[$item];
+$item1 = $test->get_items()[$item];
+$item2 = $test_prev->get_items()[$item];
 $merged = otulea::merge_items($item1, $item2);
 $cond1 = strcmp($merged['timestamp'], $timestamp) === 0;
 $cond2 = strcmp($merged['data'], $item1['data']) === 0;
