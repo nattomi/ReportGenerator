983c424d2f72fd5e2f129078e473973cca59db0a
Tamás Nagy <nattomi@ggki.hu>
2016-04-10 15:23:43 +0200

    delivers web service response

---
+++ phplib/api/response.php
@@ -0,0 +1,37 @@
+<?php
+
+class api {
+  static function response($status, $status_message, $domdocument=null) {
+    $dom = new DomDocument('1.0', 'UTF-8');
+    $results = $dom->appendChild($dom->createElement('response'));
+    $results->appendChild($dom->createElement('status', $status));
+    $results->appendChild($dom->createElement('status_message', $status_message\
+					      ));
+
+    if(is_null($domdocument)) {
+      $domdocument = new DomDocument('1.0', 'UTF-8');
+      $domdocument->appendChild($domdocument->createElement('data'));
+    }
+
+    if($domdocument->documentElement->nodeName === 'data')
+      $newdom = $domdocument;
+    else {
+      $newdom = new DOMDocument('1.0', 'UTF-8');
+      $data = $newdom->appendChild($newdom->createElement('data'));
+
+      foreach ($domdocument->documentElement->childNodes as $domElement){
+	$domNode = $newdom->importNode($domElement, true);
+        $data->appendChild($domNode);
+      }
+    }
+
+    $import = $dom->importNode($newdom->documentElement, TRUE);
+    $results->appendChild($import);
+
+    $dom->formatOutput = true;
+
+    echo $dom->saveXML();
+  }
+}
+
+?>
